e2c91176f1fc6abe0742cdcb050e7f45e5a76d0e
CliffHanger201 <sandy.hay@ntlworld.com>
2024-10-15 18:29:18 +0100

    added more comments to Job class

--- job.java
+++ job.java
@@ -9,8 +9,10 @@ public void run () {
 // Undertake required work, here we will emulate it by sleeping for a period
   System.out.println ("Job: " + jobNumber + " is being processed by thread : "
     + Thread.currentThread ().getName());
+    // This gets the name of the job
   try {
     Thread.sleep((int)(1000));
+    //Thread sleeping
   } catch (InterruptedException e) {
     // no catching as example should not experience interruptions
   }
